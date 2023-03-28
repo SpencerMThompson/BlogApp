88a2442c9f77a2eec8791e9e8c0cc00c151a97af
Spencer Thompson <st746866@southeast.edu>
2023-03-27 20:03:34 -0400

    database overhaul. login users

---
+++ src/Post.js
@@ -0,0 +1,18 @@
+export default function Post(){
+    return(
+        <div className="post">
+        <div className="image">
+        <img src="https://trendlist.org/img/images/big/nike-shinobi-by-rohil-borole-trendlist-1658694439.jpg"></img></div>
+        <div className="texts">
+        <h2>Nike Shinobi
+        Designed by Rohil Borole from Germany in 2020.
+        Uploaded to Trend List 8 months ago.</h2>
+        <p className="info">
+          <a href="" className="author">Spencer Thompson</a>
+          <time>2023-01-23</time>
+        </p>
+        <p className="summary">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+      </div>
+    );
+}
