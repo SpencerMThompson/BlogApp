8ff63ea89819e63b8f69f91a596583863d1911d7
Spencer Thompson <st746866@southeast.edu>
2023-04-16 11:31:00 -0400

    create post

--- src/Pages/CreatePost.js
+++ src/Pages/CreatePost.js
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import { useDebugValue, useState } from 'react';
+import { Navigate } from "react-router-dom";
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 
@@ -21,20 +22,42 @@ export default function CreatePost(){
     const [title, setTitle]= useState('');
     const [summary, setSummary] = useState('');
     const [content, setContent] = useState('');
+    const [files, setFiles] = useState('');
+    const [redirect, setRedirect] = useState(false);
 
+    async function createNewPost(ev){
+        const data = new FormData();
+        data.set('title', title);
+        data.set('summary', summary);
+        data.set('content', content);
+        data.set('file', files[0] )
+        ev.preventDefault();
+        const response = await fetch('http://localhost:4000/post', {
+            method: 'POST',
+            body: data,
+        });
+        if(response.ok){
+            setRedirect(true);
+        }
+    }
+
+    if(redirect){
+        return <Navigate to={'/'}/>
+    }
     return(
-        <form>
-            <input type="title" 
-            placeholder={'Title'} 
-            value={title} 
+        <form onSubmit={createNewPost}>
+            <input type="title"
+            placeholder={'Title'}
+            value={title}
             onChange={ev => setTitle(ev.target.value)}/>
-            <input type="summary" 
+            <input type="summary"
             placeholder={'Summary'}
             value={summary}
             onChange={ev => setSummary(ev.target.value)}/>
-            <input type="file"/>
-            <ReactQuill value={content} 
-            modules={modules} 
+            <input type="file"
+            onChange={ev => (setFiles(ev.target.files))}/>
+            <ReactQuill value={content}
+            modules={modules}
             formats={formats}
             onChange={newValue => setContent(newValue)}
             ></ReactQuill>
--- src/Post.js
+++ src/Post.js
@@ -1,17 +1,18 @@
-export default function Post(){
+import {format} from 'date-fns';
+
+export default function Post({title, summary, content, cover,createdAt}){
     return(
         <div className="post">
         <div className="image">
-        <img src="https://trendlist.org/img/images/big/nike-shinobi-by-rohil-borole-trendlist-1658694439.jpg"></img></div>
+        <img src={cover} alt="image"></img>
+        </div>
         <div className="texts">
-        <h2>Nike Shinobi
-        Designed by Rohil Borole from Germany in 2020.
-        Uploaded to Trend List 8 months ago.</h2>
+        <h2>{title}</h2>
         <p className="info">
-          <a href="" className="author">Spencer Thompson</a>
-          <time>2023-01-23</time>
+          <a className="author">Spencer Thompson</a>
+          <time>{format(new Date(createdAt), "MMM d, yyyy HH:mm")}</time>
         </p>
-        <p className="summary">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <p className="summary">{summary}</p>
         </div>
       </div>
     );
